9309182724fb22ea2045e08b9c5ff02cfbd61664
librarianmage <12386291+librarianmage@users.noreply.github.com>
2022-01-18 17:10:50 -0600

    Add initializr-spring project

---
+++ initializr-spring/src/main/java/org/folio/initializrspring/InitializrSpringApplication.java
@@ -0,0 +1,13 @@
+package org.folio.initializrspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializrSpringApplication.class, args);
+    }
+
+}
