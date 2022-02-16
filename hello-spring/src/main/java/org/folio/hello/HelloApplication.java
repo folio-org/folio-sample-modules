5a5d4e9d88932fef76b7115e822f4f54af40ad78
Noah Overcash <ncovercash@ua.edu>
2022-02-16 14:43:26 -0600

    hello-spring: Remove extra annotations

--- hello-spring/src/main/java/org/folio/hello/HelloApplication.java
+++ hello-spring/src/main/java/org/folio/hello/HelloApplication.java
@@ -2,13 +2,11 @@ package org.folio.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Main Spring Boot application class
  */
 @SpringBootApplication
-@EnableFeignClients
 public class HelloApplication {
     /**
      * Run the Spring Boot application
--- hello-spring/src/main/java/org/folio/hello/HelloController.java
+++ hello-spring/src/main/java/org/folio/hello/HelloController.java
@@ -2,7 +2,6 @@ package org.folio.hello;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.folio.hello.model.InlineResponse200;
 import org.folio.hello.api.HelloApi;
@@ -11,7 +10,6 @@ import org.folio.hello.api.HelloApi;
  * Main API controller
  */
 @RestController
-@RequestMapping(value = "/")
 public final class HelloController implements HelloApi {
     public static final String GREETING = "Hello, world!\n";
 
