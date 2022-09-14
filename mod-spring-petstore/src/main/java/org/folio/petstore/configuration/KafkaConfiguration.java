90b56e6f8f5de62f634d84b41e6908046a2feff3
Arghya22 <Arghya_Mitra@epam.com>
2022-09-14 11:00:19 +0530

    [FOLSPRINGB-63] changes added

--- mod-spring-petstore/src/main/java/org/folio/petstore/configuration/KafkaConfiguration.java
+++ mod-spring-petstore/src/main/java/org/folio/petstore/configuration/KafkaConfiguration.java
@@ -1,11 +1,9 @@
 package org.folio.petstore.configuration;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.folio.petstore.domain.kafka.event.PetEvent;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- mod-spring-petstore/src/main/java/org/folio/petstore/configuration/TopicConfiguration.java
+++ mod-spring-petstore/src/main/java/org/folio/petstore/configuration/TopicConfiguration.java
@@ -2,8 +2,6 @@ package org.folio.petstore.configuration;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
