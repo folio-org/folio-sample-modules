ef38f72479738cecf867808a6bd0c9a18ae1a913
Julian Ladisch <julianladisch@users.noreply.github.com>
2025-04-10 14:11:45 +0200

    FOLIO-4277: Bump dependencies for Sunflower
    https://folio-org.atlassian.net/browse/FOLIO-4277

--- mod-spring-petstore/src/test/java/org/folio/petstore/kafka/KafkaIntegrationTests.java
+++ mod-spring-petstore/src/test/java/org/folio/petstore/kafka/KafkaIntegrationTests.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(classes = ModPetstoreApplication.class)
@@ -18,7 +18,7 @@ import org.testcontainers.utility.DockerImageName;
 public class KafkaIntegrationTests {
 
   @Container
-  private static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.5.3"));
+  private static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.0"));
 
   private final String topicName = "spring-petStore";
 
