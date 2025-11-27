ace5e10f771d45f9725e0f01d68a67389e5957c9
Julian Ladisch <julianladisch@users.noreply.github.com>
2025-11-27 13:33:26 +0100

    Migrate from Vertx 4 to Vert.x 5

--- simple-vertx/src/main/java/org/folio/simple/SimpleWebService.java
+++ simple-vertx/src/main/java/org/folio/simple/SimpleWebService.java
@@ -17,7 +17,7 @@ import org.folio.okapi.common.OkapiClient;
  * @author heikki
  */
 public class SimpleWebService {
-  private static final Logger logger = LogManager.getLogger(SimpleWebService.class);
+  private static final Logger LOGGER = LogManager.getLogger(SimpleWebService.class);
   private final HttpClient httpClient;
 
   public SimpleWebService(HttpClient httpClient) {
@@ -26,10 +26,10 @@ public class SimpleWebService {
 
   // Handler for the GET requests.
   // Calls the hello module, and reports its success
-  public void get_handle(RoutingContext ctx) {
-    logger.debug("Simple: Handling a GET request. About to call the hello module");
-    OkapiClient okapiClient = new OkapiClient(httpClient, ctx);
-    logger.debug("Contacting Okapi via " + okapiClient.getOkapiUrl());
+  public void getHandle(RoutingContext ctx) {
+    LOGGER.debug("Simple: Handling a GET request. About to call the hello module");
+    var okapiClient = new OkapiClient(httpClient, ctx);
+    LOGGER.debug("Contacting Okapi via {}", okapiClient.getOkapiUrl());
     okapiClient.get("/hello")
       .onSuccess(body -> responseText(ctx, 200).end("Hello module says: '" + body + "'"))
       .onFailure(e -> responseError(ctx, 500, "Hello failed with " + e.getMessage()));
@@ -38,24 +38,24 @@ public class SimpleWebService {
   // Handler for the POST request
   // POSTs the same request to the hello module, and returns its response in a
   // new Json response
-  public void post_handle(RoutingContext ctx) {
+  public void postHandle(RoutingContext ctx) {
     if (! "application/json".equals(ctx.request().getHeader("Content-Type"))) {
       responseError(ctx, 400, "Only accepts Content-Type application/json");
       return;
     }
-    String reqData = ctx.getBodyAsString();
-    logger.debug("Simple: Received a POST of " + reqData);
-    OkapiClient okapiClient = new OkapiClient(httpClient, ctx);
+    var reqData = ctx.body().asString();
+    LOGGER.debug("Simple: Received a POST of {}", reqData);
+    var okapiClient = new OkapiClient(httpClient, ctx);
     okapiClient.setHeaders(Map.of(
         "Content-Type", "application/json",
         "X-Okapi-Tenant", ctx.request().getHeader("X-Okapi-Tenant")));
     okapiClient.post("/hello", reqData)
       .onSuccess(helloRes -> {
-        JsonObject hjo = new JsonObject(helloRes);
-        String greeting = hjo.getString("greeting");
-        logger.info("Got a greeting from hello: " + greeting);
-        hjo.put("simple", "Simple module did indeed call the hello module!");
-        String simpleRes = hjo.encodePrettily();
+        var jsonObject = new JsonObject(helloRes);
+        var greeting = jsonObject.getString("greeting");
+        LOGGER.info("Got a greeting from hello: {}", greeting);
+        jsonObject.put("simple", "Simple module did indeed call the hello module!");
+        var simpleRes = jsonObject.encodePrettily();
         responseJson(ctx, 200).end(simpleRes);
       })
       .onFailure(e -> responseError(ctx, 500, "Hello failed with " + e.getMessage()));
